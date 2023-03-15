1c42eb4182eb3d01b40b6c323e795bed2e983d3c
munene-m <munenenjue18@gmail.com>
2023-03-15 22:19:00 +0300

    began layout of about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About our CEO</h1>
+    <p>Margaret holds a Bachelor’s degree in Marketing from the Management University of Africa and a Diploma in Public Relations,  From the Kenya Institute of Management. Margaret won the Best Student Award country wide from the Kenya Institute of Management, Kenya.
+      She has over 10 years of experience in Marketing and Consultancy in various Industries.
+    </p>
+    <h2>Contacts</h2>
+    <p>+254722928160</p><p>Idealmarkconsultants@gmail.com</p>
   </div>
 </template>
 
 <style>
   .about {
     display: flex;
-    align-items: center;
+    align-items: flex-start;
+    justify-content: center;
+    flex-direction: column;
+    width: 50vw;
+    margin: auto;
+  }
+  .about h1, h2{
+    text-align: center;
+    color: blue;
+    text-decoration: underline;
   }
 </style>
